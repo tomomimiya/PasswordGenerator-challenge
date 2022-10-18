06ec051a6af667fbb8179fcdb7ee002ba7e76704
tomomimiya <tomomimiya128@gmail.com>
2022-10-17 23:57:28 -0600

    scr shot added

--- script.js
+++ script.js
@@ -1,41 +1,101 @@
 var passElement = document.getElementById("password");
-var passwordLength = prompt('How long would you like your password to be?');
-var hasLowercase = confirm('Would you like lowercase letters?');
-var hasUppercase = confirm('Would you like uppercase letters?');
-var hasSpecialChar = confirm('Would you like special characters?');
-var passwordStr = '';
+var passwordLengthAnswer = prompt(
+  "How long would you like your password to be?"
+);
+var passwordLength = parseInt(passwordLengthAnswer);
+var hasLowercase = confirm("Would you like lowercase letters?");
+var hasUppercase = confirm("Would you like uppercase letters?");
+var hasSpecialChar = confirm("Would you like special characters?");
+var passwordStr = "";
 // Assignment Code
-var generateBtn = document.querySelector('#generate');
-
-//not sure if i need this here?
-var passwordStr = getUserResponse();
+var generateBtn = document.querySelector("#generate");
 
 //Arrays
-var NUMBERS = ['1','2','3','4','5','6','7','8','9'];
-var LOWERCASE = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var UPPERCASE = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var SPCIALCHARACT = ['!','%','&','*','+','-','.','?','~'];
+var NUMBERS = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var LOWERCASE = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var UPPERCASE = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var SPCIALCHARACT = ["!", "%", "&", "*", "+", "-", ".", "?", "~"];
 
-function getUserResponse() {
+//WHEN I click the button to generate a password
+//THEN I am presented with a series of prompts for password criteria
+function generatePassword() {
   if (hasLowercase === true || hasLowercase) {
-    console.log('Password has lowercase');
+    console.log("Password has lowercase");
     // might need to put build password function?
-      function buildPassword() {
-        passwordStr = passwordStr + LOWERCASE[Math.round(Math.random() * LOWERCASE.length)]
-      }
+    function buildPassword() {
+      passwordStr =
+        passwordStr + LOWERCASE[Math.round(Math.random() * LOWERCASE.length)];
+    }
   }
 
   if (hasUppercase === true || hasUppercase) {
-    console.log('Password has uppercase');
+    console.log("Password has uppercase");
     function buildPassword() {
-      passwordStr = passwordStr + UPPERCASE[Math.round(Math.random() * UPPERCASE.length)]
+      passwordStr =
+        passwordStr + UPPERCASE[Math.round(Math.random() * UPPERCASE.length)];
     }
   }
 
   if (hasSpecialChar === true || hasSpecialChar) {
-    console.log('password has special character');
+    console.log("password has special character");
     function buildPassword() {
-      passwordStr = passwordStr + SPCIALCHARACT[Math.round(Math.random * SPCIALCHARACT.length)]
+      passwordStr =
+        passwordStr +
+        SPCIALCHARACT[Math.round(Math.random * SPCIALCHARACT.length)];
     }
   }
   return {
@@ -43,27 +103,16 @@ function getUserResponse() {
     hasLowercase,
     hasUppercase,
     hasSpecialChar,
-  }
+  };
 }
-
+//creating the final password thats displayed in the user window
 function buildPasswordStr() {
-  console.log('Building password');
+  console.log("Building password");
 
-  var passwordStr = getUserResponse();
-    //Last part of function
+  var passwordStr = generatePassword();
+  var passwordText = document.querySelector("#password");
+  //Last part of function
   passElement.textContent = passwordStr;
 }
 
-// Write password to the #password input
-//function buildPasswordStr() {
-// console.log('building password');
-
-//  var passwordStr = buildPasswordStr();
-//  var passElement = document.querySelector("#password");
-
-//  passElement.value = passwordStr;
-//}
-
-// Add event listener to generate button
-
-generateBtn.addEventListener ("click", buildPasswordStr);
+generateBtn.addEventListener("click", buildPasswordStr);
